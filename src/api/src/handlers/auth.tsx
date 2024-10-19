3a9824f2bb0b2f8f4b90ccab6a9a2b612024c1cc
Samuel Sendzul <samuel.sendzul@gmail.com>
2024-10-20 00:34:36 +0200

    add polling job

--- src/api/src/handlers/auth.tsx
+++ src/api/src/handlers/auth.tsx
@@ -6,46 +6,7 @@ import {
 } from "../../../chat-bot/src/services/loans";
 import { Layout } from "../components/Layout";
 import { Status } from "../components/Status";
-
-const sendLoanOutcomeToUser = async (chatId: number, loanId: string) => {
-  const outcomeMessage =
-    "<b>Your loan application was successful. ✅</b>\n\n You can repay using one of the following plans:\n\n• <b>Fixed Plan</b>: Fixed repayments at regular intervals.\n\n• <b>Variable Plan</b>: A percentage of all incoming deposits will be taken as repayment.\n\nPick the option that works for you!";
-
-  const options = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "Fixed",
-            callback_data: `fixed_repayments_${loanId}`,
-          },
-          {
-            text: "Variable",
-            callback_data: `variable_repayments_${loanId}`,
-          },
-        ],
-      ],
-    },
-    parse_mode: "HTML",
-  };
-
-  const telegramApiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
-  const response = await fetch(telegramApiUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: outcomeMessage,
-      ...options,
-    }),
-  });
-
-  if (!response.ok) {
-    console.error("Failed to send message to Telegram:", response.statusText);
-  }
-};
+import { sendLoanOutcomeToUser } from "../services/notifications";
 
 export const handleInteraction = async (c: Context) => {
   const interactRef = c.req.query("interact_ref");
